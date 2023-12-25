ffbd242ac662c8aa4f8ec6c4c117518fc98d4541
Krisler <wendellsatney@live.com>
2023-12-25 13:56:05 +0000

    bitwise

---
+++ C++/Cpp RoadMap/bitwise/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+
+    int result = 5 & 3; //AND
+    int result1 = 5 | 3; //OR
+    int result2 = 5 ^ 3; //XOR
+    int result3 = 5 << 1; //left shift
+    int result4 = 5 >> 1; //right shift
+    cout<<result1<<endl;
+    return 0;
+}
