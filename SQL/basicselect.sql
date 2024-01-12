5e1172f17f80f78e38f16676a51be00923cd1923
Krisler <wendellsatney@live.com>
2024-01-12 05:38:37 -0800

    sql

--- SQL/basicselect.sql
+++ SQL/basicselect.sql
@@ -4,10 +4,42 @@ Select * from city where population > 100000 and countrycode = 'USA';
 
 /*Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.*/
 SELECT ( (
-            SELECT COUNT(CITY)
+            SELECT
+                COUNT(CITY)
             FROM STATION
         ) - (
-            SELECT COUNT(DISTINCT CITY)
+            SELECT
+                COUNT(DISTINCT CITY)
             FROM STATION
         )
     );
+
+/*Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths 
+ (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.*/
+
+select t.city, t.lent
+from (
+        select
+            city,
+            len(city) lent
+        from station
+    ) t
+where t.lent in (
+        select max(len(city))
+        from station
+    )
+union
+select t.city, t.lent
+from (
+        select
+            city,
+            len(city) lent
+        from station
+    ) t
+where t.lent in (
+        select min(len(city))
+        from station
+    )
+order by lent desc
+OFFSET
+    0 ROWS FETCH FIRST 2 ROWS ONLY;
