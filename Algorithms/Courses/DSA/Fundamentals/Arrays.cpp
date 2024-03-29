82d6d63efb1d3e8e2283a6172cac9259683af07b
Krisler <wendellsatney@live.com>
2024-03-29 13:56:15 -0700

    DSA Fundamentals - Arrays

---
+++ Algorithms/Courses/DSA/Fundamentals/Arrays.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+
+  int A[5] = {2, 4, 6, 8, 10};
+
+  for (int i = 0; i < 5; i++)
+  {
+    cout << A[i] << " ";
+  }
+  // cout << sizeof(A) << endl;
+  // cout << A[1] << endl;
+  // printf("%d\n", A[4]);
+  return 0;
+}
